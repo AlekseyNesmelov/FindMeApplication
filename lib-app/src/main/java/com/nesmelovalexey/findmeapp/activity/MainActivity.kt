a4c0f4b17d6182189a85f535ca2022bb1e565951
alexeynesmelov <Nesmelov.A.Ev@sberbank.ru>
2019-03-25 19:23:20 +0300

    added main screen animation

--- lib-app/src/main/java/com/nesmelovalexey/findmeapp/activity/MainActivity.kt
+++ lib-app/src/main/java/com/nesmelovalexey/findmeapp/activity/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.nesmelovalexey.findmeapp.activity
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.nesmelovalexey.findmeapp.R
 import com.nesmelovalexey.maplib.MapFragment
 import kotlinx.android.synthetic.main.main_activity.*
 
-
+// TODO REFACTOR!!!
 class MainActivity : AppCompatActivity() {
+
+    private val scaleFactor = 8f
 
     // TODO viewModel
 
@@ -34,6 +40,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
 
         navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+
+        drawerLayout.setScrimColor(Color.TRANSPARENT)
+        val actionBarDrawerToggle = object : ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close) {
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+                super.onDrawerSlide(drawerView, slideOffset)
+                val slideX = drawerView.width * slideOffset
+                container.translationX = slideX
+                container.scaleX = 1 - (slideOffset / scaleFactor)
+                container.scaleY = 1 - (slideOffset / scaleFactor)
+            }
+        }
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
     }
 
     private fun replaceFragment(newFragment: Fragment, tag: String) {
